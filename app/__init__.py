46c0685e0fec94255fe6cd3d6de0a5825f17e757
Your GitHub Username <your.github.email@example.com>
2025-06-21 06:35:10 +0530

    Update: IST time support, responsive UI, and push instance folder

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
+from app.utils import format_ist_time_full, format_ist_time_medium, format_ist_time_short, format_ist_date, format_ist_date_short, format_ist_time_receipt, format_ist_receipt_id
 
 load_dotenv()
 
@@ -23,6 +24,15 @@ def create_app():
 
     login_manager.login_view = 'main.login'
     login_manager.login_message_category = 'info'
+
+    # Register custom Jinja2 filters for IST time formatting
+    app.jinja_env.filters['ist_full'] = format_ist_time_full
+    app.jinja_env.filters['ist_medium'] = format_ist_time_medium
+    app.jinja_env.filters['ist_short'] = format_ist_time_short
+    app.jinja_env.filters['ist_date'] = format_ist_date
+    app.jinja_env.filters['ist_date_short'] = format_ist_date_short
+    app.jinja_env.filters['ist_receipt'] = format_ist_time_receipt
+    app.jinja_env.filters['ist_receipt_id'] = format_ist_receipt_id
 
     from app.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
